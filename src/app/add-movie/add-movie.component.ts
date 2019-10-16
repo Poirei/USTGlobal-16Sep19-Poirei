d76646729be76b8193918ffc424113f5bf9cb66d
K.Poirei Ngamba Singha <poirei4@gmail.com>
2019-10-16 22:53:27 +0530

    Add files via upload

--- src/app/add-movie/add-movie.component.ts
+++ src/app/add-movie/add-movie.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-movie',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-movie.component.css']
 })
 export class AddMovieComponent implements OnInit {
+  get email() {
+    return this.form.get('email');
+  }
+  get password() {
+    return  this.form.get('password');
+  }
+
+  get imgUrl() {
+    return this.form.get('imgUrl');
+  }
+
+  get description() {
+    return this.form.get('description');
+  }
+
+  form = new FormGroup({
+    email : new FormControl('' , [Validators.required ]),
+    password : new FormControl('' , [Validators.required ]),
+    imgUrl : new FormControl('', [Validators.required ]),
+    description : new FormControl('', [Validators.required])
+  });
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  loginData(form) {
+    console.log(form);
+  }
 }
